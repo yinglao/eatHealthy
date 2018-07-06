a23fd97cf8b89c938b8e6eb03fa0b4246fa2fe95
Yinglao Liu <yl3596@columbia.edu>
2018-07-06 16:39:38 -0700

    start hw 4

---
+++ eclipse-workspace/ReviewApp/src/review/dal/UsersDao.java
@@ -0,0 +1,31 @@
+package review.dal;
+
+import review.model.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+
+public class UsersDao {
+	protected ConnectionManager connectionManager;
+	private static UsersDao instance = null;
+	protected UsersDao() { 
+		connectionManager = new ConnectionManager();
+	}
+	public static UsersDao getInstance() {
+		if(instance == null) {
+			instance = new UsersDao();
+		}
+		return instance;
+	}
+	
+	
+
+}
---
+++ eclipse-workspace/ReviewApp/src/review/model/Users.java
@@ -0,0 +1,111 @@
+package review.model;
+
+public class Users {
+	protected String userName;
+	protected String password;
+	protected String firstName;
+	protected String lastName;
+	protected String email;
+	protected String phone;
+	/**
+	 * @param userName
+	 * @param password
+	 * @param firstName
+	 * @param lastName
+	 * @param email
+	 * @param phone
+	 */
+	public Users(String userName, String password, String firstName, String lastName, String email, String phone) {
+		this.userName = userName;
+		this.password = password;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.phone = phone;
+	}
+	/**
+	 * @param userName
+	 */
+	public Users(String userName) {
+		this.userName = userName;
+	}
+	/**
+	 * @return the userName
+	 */
+	public String getUserName() {
+		return userName;
+	}
+	/**
+	 * @param userName the userName to set
+	 */
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+	/**
+	 * @param firstName the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+	/**
+	 * @param lastName the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	/**
+	 * @return the phone
+	 */
+	public String getPhone() {
+		return phone;
+	}
+	/**
+	 * @param phone the phone to set
+	 */
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	
+	
+
+	
+	
+
+}
